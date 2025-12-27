ac2a89204cfa29145d5930faaf60f33cac6d3c7c
bhawesh22 <mail2bhaweshmishra@gmail.com>
2025-12-27 16:34:54 +0530

    Update provider.tf

--- Environment/provider.tf
+++ Environment/provider.tf
@@ -6,9 +6,15 @@ terraform {
       version = "4.34.0"
     }
   }
+backend "azurerm" {
+    resource_group_name  = "bhaweshnew"
+    storage_account_name = "bhaweshmishra"
+    container_name       = "bhaweshcontainer"
+    key                  = "bhawesh.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "35e5fe36-f38c-45e9-bc91-ab9e1660e448"
+  subscription_id = "18bbb1ca-f79e-49b4-8669-5a1208da00f7"
 } 
